ac01edd137cb7cfbb3b2e27e7b09f9486fe81b79
Prem Kumar <M1039269@A2ML23770>
2019-07-25 20:59:41 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { GoogleLogout } from "react-google-login";
+
+import "./App.scss";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userDetails: {},
+      isUserLoggedIn: false
+    };
+  }
+
+  responseGoogle = response => {
+    this.setState({ userDetails: response.profileObj, isUserLoggedIn: true });
+  };
+
+  logout = () => {
+    this.setState({isUserLoggedIn: false})
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {!this.state.isUserLoggedIn && (
+          <GoogleLogin
+            clientId="269203047608-nket5dj96lnf0fpt5ds19gad0u59qi6h.apps.googleusercontent.com"
+            render={renderProps => (
+              <button
+                className="button"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                Log in with Google
+              </button>
+            )}
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+          />
+        )}
+        {this.state.isUserLoggedIn && (
+          <div className="userDetails-wrapper">
+            <div className="details-wrapper">
+              <GoogleLogout
+                render={renderProps => (
+                  <button
+                    className="logout-button"
+                    onClick={renderProps.onClick}
+                  >
+                    Log Out
+                  </button>
+                )}
+                onLogoutSuccess={this.logout}
+              />
+
+              <div className="image">
+                <img src={this.state.userDetails.imageUrl} />
+              </div>
+              <div className="name">
+                Welcome Mr. {this.state.userDetails.givenName}{" "}
+                {this.state.userDetails.familyName}
+              </div>
+              <div className="email"><i>{this.state.userDetails.email}</i></div>
+            </div>
+            <div className="bar" />
+            <div className="stand" />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
